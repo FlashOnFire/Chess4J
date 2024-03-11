e088b647efc5d4eee697b5915da30f034fc67a9d
eymeric <eymericdechelette@gmail.com>
2024-03-11 16:10:35 +0100

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,15 @@
+plugins {
+    id("java")
+}
+
+group = "fr.onyx"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+//    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+//    testImplementation("org.junit.jupiter:junit-jupiter")
+}
---
+++ src/main/java/fr/onyx/Main.java
@@ -0,0 +1,7 @@
+package fr.onyx;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
